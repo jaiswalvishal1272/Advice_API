5fa26597d3429e7dd622ad5bf6d69ee1bb10cd84
jaiswalvishal1272 <jaiswalvishal1272.gmail.com>
2023-07-02 20:34:26 +0530

    Cors module included.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 
 const port = process.env.PORT;
+
+app.use(cors());
 
 app.use("/", require("./routes/adviceRoutes"));
 
