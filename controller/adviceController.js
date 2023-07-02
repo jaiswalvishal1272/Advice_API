b1491e83d4d5996788bfae414c589720734c7fcf
jaiswalvishal1272 <jaiswalvishal1272.gmail.com>
2023-07-02 19:16:39 +0530

    Routing and Controllers are introduced.

---
+++ controller/adviceController.js
@@ -0,0 +1,16 @@
+const conn = require("../config/dbConnection");
+
+//@desc Get advice
+//@route /
+//@access public
+const getAdvice = async (req, resp) => {
+    // get a random ID
+    const id = Math.floor(Math.random() * 10) + 1;
+    // fetch the data from database
+    conn.query("SELECT ADVICE FROM advices WHERE ID = ?", id, (err, data) => {
+        if(err) resp.send(err);
+        else resp.send(data);
+    })
+};
+
+module.exports = getAdvice;
