618077e8634a93121b082733fddd069cba2e850f
jaiswalvishal1272 <jaiswalvishal1272.gmail.com>
2023-09-30 00:18:37 +0530

    vercel file created for deployment.

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.use(cors());
 app.use("/", require("./routes/adviceRoutes"));
 
 // Calling HTTP server
-app.listen(port , () => {
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
